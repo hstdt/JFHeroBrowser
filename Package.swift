2ad3079075ab19817882d4b8ab1f677c4ab11e51
tiandt <hstdt@users.noreply.github.com>
2025-03-13 14:08:13 +0800

    BaseKit使用SPM

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/JerryFans/JRBaseKit", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,10 +26,6 @@ let package = Package(
         .target(
             name: "JFHeroBrowser",
             dependencies: ["JRBaseKit"],
-            resources: [.process("Resources/Assets")]),
-        .target(
-            name: "JRBaseKit",
-            dependencies: [],
-            sources: ["Classes"])
+            resources: [.process("Resources/Assets")])
     ]
 )
